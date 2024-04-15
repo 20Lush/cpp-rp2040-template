57ea4cd4a49ba5ef344bba12fc35db528add4291
Zachary Johnson <zachfsx83@gmail.com>
2024-04-14 21:17:49 -0700

    [Opensauce] Finish it out

--- firmware/include/main.hpp
+++ firmware/include/main.hpp
@@ -1,1 +1,9 @@
 
+/** @brief A general project namespace scope for functions used far and wide.
+ *  @note Better to put things here over global scope.
+*/
+namespace lush::rp2040 {
+
+    void printHelloWorld();
+
+}
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,16 +1,31 @@
-
-#include <stdio.h>
 #include <pico/stdlib.h>
+#include <iostream>
 
-#include <stdio.h>
-#include "tusb.h"
-#include "pico/stdlib.h"
+#include "main.hpp"
 
+/** Define the prototypes in this namespace (probably should do this in a different file).
+ *  If you do make another file, make sure to add it's c/cpp file to the CMakeLists.txt SOURCE_FILE setter
+ *  */ 
+namespace lush::rp2040 {
+
+    void printHelloWorld() {
+        std::cout << "Hello, World!" << std::endl;
+    }
+
+}
+
+/** @brief The entry point of the program
+ *  @note  Args not included, and I recommend not even bothering with it :)
+ *  @returns Probably shouldn't return unless something bad has gone wrong and you have a debugger attached to see it 
+*/
 int main() {
+
     stdio_init_all();
 
     while (true) {
-        printf("Hello, world!\n");
+
+        lush::rp2040::printHelloWorld();
+
         sleep_ms(1000);
     }
 }
